e17ceaf028d893696f089b8573396d3ea12de13e
Ben Boeckel <mathstuf@gmail.com>
2015-12-10 11:08:34 -0500

    vtk: update to 7.0.0-RC1

--- CMake/vtk_version.cmake
+++ CMake/vtk_version.cmake
@@ -1,9 +1,9 @@
 # We hardcode the version numbers since we cannot determine versions during
 # configure stage.
-set (vtk_version_major 6)
-set (vtk_version_minor 3)
+set (vtk_version_major 7)
+set (vtk_version_minor 0)
 set (vtk_version_patch 0)
-#set (vtk_version_suffix "RC1")
+set (vtk_version_suffix "RC1")
 set (vtk_version "${vtk_version_major}.${vtk_version_minor}")
 if (vtk_version_suffix)
   set (vtk_version_long "${vtk_version}.${vtk_version_patch}-${vtk_version_suffix}")
