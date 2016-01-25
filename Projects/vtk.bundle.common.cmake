5dd71737dd8b44f2c5e41a521e27ee9c31a94d91
Dave DeMarle <dave.demarle@kitware.com>
2016-01-25 18:51:24 -0500

    change package name to reflect what we put on website
    specifically call it
    vtkpython-M.n.p.rcN-ladida instead of
    VTK-M.n.p-RCN-ladida

--- Projects/vtk.bundle.common.cmake
+++ Projects/vtk.bundle.common.cmake
@@ -18,13 +18,14 @@ set(CPACK_PACKAGE_VENDOR "Kitware, Inc.")
 set(CPACK_PACKAGE_VERSION_MAJOR ${vtk_version_major})
 set(CPACK_PACKAGE_VERSION_MINOR ${vtk_version_minor})
 if (vtk_version_suffix)
-  set(CPACK_PACKAGE_VERSION_PATCH ${vtk_version_patch}-${vtk_version_suffix})
+  string(TOLOWER "${vtk_version_suffix}" lower_vtk_version_suffix)
+  set(CPACK_PACKAGE_VERSION_PATCH ${vtk_version_patch}.${lower_vtk_version_suffix})
 else()
   set(CPACK_PACKAGE_VERSION_PATCH ${vtk_version_patch})
 endif()
 
 set(CPACK_PACKAGE_FILE_NAME
-    "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}-${package_suffix}")
+    "vtkpython-${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}-${package_suffix}")
 
 # Don't import CPack yet, let the platform specific code get another chance at
 # changing the variables.
