2d70aad52c89ed852badad1cf1ce24f43cdb0b39
Ben Boeckel <mathstuf@gmail.com>
2016-01-28 14:13:22 -0500

    versions: fix tag name

--- versions.cmake
+++ versions.cmake
@@ -72,7 +72,7 @@ cmake_dependent_option(BUILD_VTK7 "Build VTK 7" ON
 if (USE_VTK_MASTER)
   set(vtk_tag "master")
 else ()
-  set(vtk_tag "${vtk_version_tag}")
+  set(vtk_tag "v${vtk_version_tag}")
 endif ()
 add_revision(vtk
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/vtk.git
